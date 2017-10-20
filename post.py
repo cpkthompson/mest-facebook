357b788aa4764809cfff42d79216ce0391c4d3de
ehiaig <ehiagheaigg@gmail.com>
2017-10-20 16:48:49 +0000

    Restructered the facebook clone into a modular code

--- post.py
+++ post.py
@@ -1,35 +1,31 @@
-# import user
+import user
 import csv
 import os
 
 class Post:
 
-    def __init__(self,  firstname, content=""):
-        self.firstname = firstname
+    def __init__(self,  email, content=""):
+        self.email = email
         self.content = content
-
 
     def save(self):
         file_exist = os.path.isfile('posts.csv')
         with open('posts.csv', 'a') as my_file:
-            fieldnames = ['username', 'content']
+            fieldnames = ['Email', 'content']
             writer = csv.DictWriter(my_file, fieldnames=fieldnames)
 
             if not file_exist:
                 writer.writeheader()
-            writer.writerow({'username': self.firstname, 'content': self.content})
+            writer.writerow({'Email': self.email, 'content': self.content})
 
-    def show_all_posts(self, firstname):
+    def show_all_posts(self):
         post = open('posts.csv', 'r')
         post_reader =  csv.DictReader(post)
-        for line in post_reader:
-            #print(line)
-            if firstname in line.values():
-                print (line)
-
-class Author:
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return self.name
+        for postss in post_reader:
+            if self.email == postss['Email']:
+                print('/n')
+                print('**** Here are your Posts****')
+                print(postss['Content'])
+            # if self.email in postss.values():
+            #     print (postss)
+        print('What would like to do next?')
--- profile.py
+++ profile.py
@@ -1,19 +1,21 @@
-from post import Post, Author
+from post import Post
 # from user import User
 # from fb_page import create_page
 import csv
 import os
 
-def create_post(content, firstname="Anonymous"):
+# print('**** Welcome {}'.format(User.first_name))
+def create_post(email):
     '''User can create a post'''
-    p = Post(firstname, content)
+    content = input("What/'s on your mind?: ")
+    p = Post(email, content)
     p.save()
     print('Post created')
 
-def display_posts(firstname):
+def display_posts(email):
     '''This displays users posts'''
-    d = Post(firstname)
-    d.show_all_posts(firstname)
+    d = Post(email)
+    d.show_all_posts()
 
 def update_bio(bio_details, firstname='charles'):
     '''User can update their bio'''
@@ -29,33 +31,6 @@ def create_pg():
     # fb_page.create_page()
     pass
 
-
-if __name__ == '__main__':
-
-    # This displays the users post. This function is called first in this class
-    display_posts('charles')
-
-    #We are asking the user what they want to do
-    quit = True
-    while quit:
-        choice = input(" 1. to Create Post\n 2. to Update Bio\n 3. to Create a Page\n 4. to logout")
-        if choice == '1':
-            # This lets user creates a posts
-            content = str(input('Enter post: '))
-            create_post(content)
-
-        elif choice=='2':
-            # This let's the user update bio
-            bio_details = input("What\'s your fun facts?: ")
-            update_bio(bio_details)
-
-        elif choice == '3':
-            #This lets user create page
-            create_pg()
-
-        elif choice == '4':
-            #This logs the user out of the application
-            quit = False
 
 
 
